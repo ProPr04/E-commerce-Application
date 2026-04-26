48dec9565a45603bd4462df3372ef56ea3230638
Pranit <pranitjadhav0407@gmail.com>
2026-04-26 14:11:55 +0530

    Configure axios for deployed backend

--- ecommerce-frontend/src/api/axios.js
+++ ecommerce-frontend/src/api/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+const deployedBackendUrl = "https://e-commerce-application-ren0.onrender.com";
+const isLocalhost =
+  typeof window !== "undefined" &&
+  ["localhost", "127.0.0.1"].includes(window.location.hostname);
+
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:5000",
+    (isLocalhost ? "http://localhost:5000" : deployedBackendUrl),
 });
 
 API.interceptors.request.use((req) => {
