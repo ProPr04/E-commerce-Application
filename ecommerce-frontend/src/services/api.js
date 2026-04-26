a5c19fc9c3a7351be64caf592119508dc674c23c
Pranit <pranitjadhav0407@gmail.com>
2026-04-26 15:02:26 +0530

    backend fix

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -1,11 +1,12 @@
 import API from "../api/axios";
 
 export const fetchProducts = async () => {
-  const response = await API.get("/products");
+  const response = await API.get("https://e-commerce-application-ren0.onrender.com/products");
   return response.data;
 };
 
 export const fetchProductById = async (id) => {
-  const response = await API.get(`/products/${id}`);
+  const response = await API.get(`https://e-commerce-application-ren0.onrender.com/products/${id}`);
+  console.log(response)
   return response.data;
 };
