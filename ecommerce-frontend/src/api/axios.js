b0e416f2af8fcfa8d42b6446e69d57adfa213506
Pranit <pranitjadhav0407@gmail.com>
2026-04-26 14:39:55 +0530

    Backend Problem Fixed

--- ecommerce-frontend/src/api/axios.js
+++ ecommerce-frontend/src/api/axios.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-const deployedBackendUrl = "https://e-commerce-application-ren0.onrender.com";
-const isLocalhost =
-  typeof window !== "undefined" &&
-  ["localhost", "127.0.0.1"].includes(window.location.hostname);
 
+
+console.log(process.env.REACT_APP_API_BASE_U)
 const API = axios.create({
   baseURL:
-    process.env.REACT_APP_API_BASE_URL ||
-    (isLocalhost ? "http://localhost:5000" : deployedBackendUrl),
+  "https://e-commerce-application-ren0.onrender.com"
+    ,
 });
 
 API.interceptors.request.use((req) => {
