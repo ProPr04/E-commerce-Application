a132bf7cfb2dcffe6421ed3fa77acb3c214d65d2
Pranit <pranitjadhav0407@gmail.com>
2026-04-26 11:29:58 +0530

    Fix production port

--- server/index.js
+++ server/index.js
@@ -46,6 +46,8 @@ app.get("/protected", authMiddleware, (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
